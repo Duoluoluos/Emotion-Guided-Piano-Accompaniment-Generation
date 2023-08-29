c18e5a7b2e72aebc7568b4021d1214204602d4fa
Qi Wang <79775004+Duoluoluos@users.noreply.github.com>
2023-08-30 03:09:12 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,5 +4,5 @@ from config import *
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 #arousal:0~100
-My_acc = EG_ACC(input_melody_folder=INPUTS_PATH,save_folder=OUTPUTS_PATH,arousal=10)
-My_acc.solve()
+My_acc = EG_ACC(input_melody_folder=INPUTS_PATH,save_folder=OUTPUTS_PATH)
+My_acc.solve(arousal=10)
